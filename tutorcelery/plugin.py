00e7d8120944aa5b0bd502f5b8bfc6aca1cc16c4
Cristhian Garcia <crisgarta8@gmail.com>
2024-08-12 12:37:48 -0500

    feat: allow to customize flower docker image

--- tutorcelery/plugin.py
+++ tutorcelery/plugin.py
@@ -26,6 +26,8 @@ hooks.Filters.CONFIG_DEFAULTS.add_items(
         ("CELERY_LMS_EXPLICIT_QUEUES", {}),
         ("CELERY_CMS_EXPLICIT_QUEUES", {}),
         ("CELERY_FLOWER", False),
+        ("CELERY_FLOWER_HOST", "flower.{{LMS_HOST}}"),
+        ("CELERY_FLOWER_DOCKER_IMAGE", "docker.io/mher/flower:2.0.1"),
     ]
 )
 
